f642af2c7dd3e0f12928f12c8f22ab5b45aef4fa
Rishika-1603 <ritzverma202@gmail.com>
2023-07-12 02:46:41 +0530

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { ProjectInterface } from "@/common.types";
+import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 
 import { fetchAllProjects } from "@/lib/actions";
-// type SearchParams = {
-//     category?: string | null;
-//     endcursor?: string | null;
-//   }
+type SearchParams = {
+    category?: string | null;
+    endcursor?: string | null;
+  }
   
-//   type Props = {
-//     searchParams: SearchParams
-//   }
+  type Props = {
+    searchParams: SearchParams
+  }
 type ProjectSearch={
     projectSearch :{
         edges : {node: ProjectInterface} [];
@@ -21,22 +23,29 @@ type ProjectSearch={
         };
     };
 }
-const Home = async () => {
 
-    const data= await fetchAllProjects() as ProjectSearch
+export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category,endcursor} }: Props) => {
+  
+
+    const data= await fetchAllProjects(category,endcursor) as ProjectSearch
     const projectsToDisplay = data?.projectSearch?.edges || [];
 
     if (projectsToDisplay.length==0) {
         <section className="flexStart flex-col paddings">
-            categories
+           <Categories/> 
             <p className="no-result-text text-center ">
               No courses found 💀
             </p>
         </section>
     }
+    const pagination= data?.projectSearch?.pageInfo ;
     return(
         <section className="flex-start flex-col paddings mb-16">
-              <h1>Categories</h1>
+              <Categories/>
               <section className="projects-grid">
                 {projectsToDisplay.map(({node }: {node: ProjectInterface}) => (
                     <ProjectCard
@@ -49,7 +58,12 @@ const Home = async () => {
                     userId={node?.createdBy?.id}/>
                 ))}
               </section>
-              <h1>LoadMore</h1>
+             <LoadMore
+                startCursor={pagination.startCursor} 
+                endCursor={pagination.endCursor} 
+                hasPreviousPage={pagination.hasPreviousPage} 
+                hasNextPage={pagination.hasNextPage}
+            />
         </section>
             
         
--- lib/actions.ts
+++ lib/actions.ts
@@ -56,10 +56,10 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
   }
 };
 
-export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
+export const fetchAllProjects = (category?: string | null, endCursor?: string | null) => {
    client.setHeader("x-api-key", apiKey);
 
-   return makeGraphQLRequest(projectsQuery, { category, endcursor });
+   return makeGraphQLRequest(projectsQuery, { category, endCursor });
  };
 
  export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
@@ -81,58 +81,9 @@ export const fetchAllProjects = (category?: string | null, endcursor?: string | 
     return makeGraphQLRequest(createProjectMutation, variables);
   }};
 
-// export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
-//   const imageUrl = await uploadImage(form.image);
 
-//   if (imageUrl.url) {
-//     client.setHeader("Authorization", `Bearer ${token}`);
 
-//     const variables = {
-//       input: { 
-//         ...form, 
-//         image: imageUrl.url, 
-//         createdBy: { 
-//           link: creatorId 
-//         }
-//       }
-//     };
 
-//     return makeGraphQLRequest(createProjectMutation, variables);
-//   }
-//};
-
-// export const updateProject = async (form: ProjectForm, projectId: string, token: string) => {
-//   function isBase64DataURL(value: string) {
-//     const base64Regex = /^data:image\/[a-z]+;base64,/;
-//     return base64Regex.test(value);
-//   }
-
-//   let updatedForm = { ...form };
-
-//   const isUploadingNewImage = isBase64DataURL(form.image);
-
-//   if (isUploadingNewImage) {
-//     const imageUrl = await uploadImage(form.image);
-
-//     if (imageUrl.url) {
-//       updatedForm = { ...updatedForm, image: imageUrl.url };
-//     }
-//   }
-
-//   client.setHeader("Authorization", `Bearer ${token}`);
-
-//   const variables = {
-//     id: projectId,
-//     input: updatedForm,
-//   };
-
-//   return makeGraphQLRequest(updateProjectMutation, variables);
-// };
-
-// export const deleteProject = (id: string, token: string) => {
-//   client.setHeader("Authorization", `Bearer ${token}`);
-//   return makeGraphQLRequest(deleteProjectMutation, { id });
-// };
 
 
 export const getProjectDetails = (id: string) => {
@@ -163,8 +114,40 @@ export const getUser = (email: string) => {
 
 
 
-// export const getUserProjects = (id: string, last?: number) => {
-//   client.setHeader("x-api-key", apiKey);
-//   return makeGraphQLRequest(getProjectsOfUserQuery, { id, last });
-// };
+export const getUserProjects = (id: string, last?: number) => {
+  client.setHeader("x-api-key", apiKey);
+  return makeGraphQLRequest(getProjectsOfUserQuery, { id, last });
+};
 
+export const deleteProject = (id: string, token: string) => {
+  client.setHeader("Authorization", `Bearer ${token}`);
+  return makeGraphQLRequest(deleteProjectMutation, { id });
+};
+
+ export const updateProject = async (form: ProjectForm, projectId: string, token: string) => {
+  function isBase64DataURL(value: string) {
+    const base64Regex = /^data:image\/[a-z]+;base64,/;
+    return base64Regex.test(value);
+  }
+
+  let updatedForm = { ...form };
+
+ const isUploadingNewImage = isBase64DataURL(form.image);
+
+  if (isUploadingNewImage) {
+    const imageUrl = await uploadImage(form.image);
+
+    if (imageUrl.url) {
+      updatedForm = { ...updatedForm, image: imageUrl.url };
+    }
+  }
+
+  client.setHeader("Authorization", `Bearer ${token}`);
+
+  const variables = {
+    id: projectId,
+    input: updatedForm,
+  };
+
+  return makeGraphQLRequest(updateProjectMutation, variables);
+};
