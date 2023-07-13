5f0b094e5c5350fce532d03e5a3ef09689b6e2fd
Rishika-1603 <ritzverma202@gmail.com>
2023-07-13 16:58:19 +0530

    ....

--- graphql/index.ts
+++ graphql/index.ts
@@ -57,7 +57,7 @@ export const createUserMutation = `
 
 export const projectsQuery = `
   query getProjects($category: String, $endcursor: String) {
-    projectSearch(first: 8, after: $encursor, filter: {category: {eq: $category}}) {
+    projectSearch(first: 8, after: $endcursor, filter: {category: {eq: $category}}) {
       pageInfo {
         hasNextPage
         hasPreviousPage
